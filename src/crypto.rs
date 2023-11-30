61af0e666b9cd175ef4b20ae3a1c127c46831ac1
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2023-11-30 19:05:44 +0000

    Now trying to figure out more conversions.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,7 +1,7 @@
 //! Compatibility layers for NistP384 elliptic curve keypairs.
 
 
-use elliptic_curve::{PublicKey, SecretKey, pkcs8::EncodePrivateKey};
+use elliptic_curve::{PublicKey, SecretKey, pkcs8::{EncodePrivateKey, DecodePrivateKey}};
 use p384::NistP384;
 use rand_core::CryptoRngCore;
 use rcgen::DistinguishedName;
@@ -12,6 +12,8 @@ pub struct KeyPair(SecretKey<NistP384>, PublicKey<NistP384>);
 
 
 impl KeyPair {
+    
+
     // Generates a new, random keypair from a RNG
     pub fn random(rng: &mut impl CryptoRngCore) -> Self {
 
@@ -29,9 +31,9 @@ impl KeyPair {
         self.1.clone().into()
     }
 
-    /// Create an x509 CA from this certificate, using the given distinguished name
+    /// Create an x509 certificate from this keypair, using the given distinguished name
     /// and alt names. Requires the PKCS_ECDSA_P384_SHA384 algorithm
-    pub fn to_x509_ca(&self, distinguished_name: DistinguishedName, subject_alt_names: impl Into<Vec<String>>) -> rcgen::Certificate {
+    pub fn to_x509(&self, distinguished_name: DistinguishedName, subject_alt_names: impl Into<Vec<String>>, is_ca: rcgen::IsCa) -> rcgen::Certificate {
         
 
         // Default params
@@ -44,7 +46,7 @@ impl KeyPair {
         cert_params.distinguished_name = distinguished_name;
 
         // Set it as a CA
-        cert_params.is_ca = rcgen::IsCa::Ca(rcgen::BasicConstraints::Unconstrained);
+        cert_params.is_ca = is_ca;
 
         // Set the keypair
         cert_params.key_pair = Some(self.try_into().unwrap());
@@ -52,32 +54,15 @@ impl KeyPair {
         // Generate the certificate
         rcgen::Certificate::from_params(cert_params).unwrap()
     }
+    
+    /// Loads the keypair from an x509 certificate
+    pub fn from_x509(cert: rcgen::Certificate) -> Self {
+        // Get the rcgen keypair
+        let keypair = cert.get_key_pair();
 
-    /// Given an x509 CA certificate, a distinguished name, and some alt names, return a
-    /// DER-serialized certificate for this keypair
-    pub fn to_certificate(&self, ca: &rcgen::Certificate, distinguished_name: DistinguishedName, subject_alt_names: impl Into<Vec<String>>) -> Vec<u8> {
+        // Convert into ourself
+        keypair.try_into().unwrap()
 
-
-        // Default params
-        let mut cert_params = rcgen::CertificateParams::new(subject_alt_names);
-
-        // PKCS_ECDSA_P384_SHA384
-        cert_params.alg = &rcgen::PKCS_ECDSA_P384_SHA384;
-
-        // Distinguished name
-        cert_params.distinguished_name = distinguished_name;
-
-        // Set it as a CA
-        cert_params.is_ca = rcgen::IsCa::ExplicitNoCa;
-
-        // Set the keypair
-        cert_params.key_pair = Some(self.try_into().unwrap());
-
-        // Generate the certificate
-        let cert = rcgen::Certificate::from_params(cert_params).unwrap();
-
-        // Serialize and sign
-        cert.serialize_der_with_signer(ca).unwrap()
     }
 }
 
@@ -95,22 +80,6 @@ impl From<SecretKey<NistP384>> for KeyPair {
 #[derive(Debug)]
 pub struct ConversionError;
 
-impl TryFrom<KeyPair> for rcgen::KeyPair {
-    type Error = ConversionError;
-
-    fn try_from(value: KeyPair) -> Result<Self, Self::Error> {
-        // First create a PCKS8 document
-        let key_doc = value.signing_key().to_pkcs8_der()
-            .map_err(|_| ConversionError)?;
-        
-        // serialize it to ANS.1 DER
-        let key_der = key_doc.to_bytes();
-
-        // Create a rcgen::KeyPair
-        rcgen::KeyPair::from_der(&key_der)
-            .map_err(|_| ConversionError)
-    }
-}
 
 
 impl TryFrom<&KeyPair> for rcgen::KeyPair {
@@ -130,5 +99,24 @@ impl TryFrom<&KeyPair> for rcgen::KeyPair {
     }
 }
 
+
+
+impl TryFrom<&rcgen::KeyPair> for KeyPair {
+    type Error = ConversionError;
+
+    fn try_from(value: &rcgen::KeyPair) -> Result<Self, Self::Error> {
+        // Get the DER serialized keypair
+        let der = value.serialized_der();
+
+        // Load the secret key
+        let secret = SecretKey::from_pkcs8_der(der)
+        .map_err(|_| ConversionError)?;
+        
+        // Get the public key
+        let public = secret.public_key();
+
+        Ok(Self(secret, public))
+    }
+}
 
 
