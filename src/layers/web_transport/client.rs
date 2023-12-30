21a316531ec9ea1fdc4b6c30ee56115ce96f2592
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2023-12-30 10:55:40 -0500

    Thinking about how different connections are going to work. Most likely will have connections to namespaces initiated by layers.

--- src/layers/web_transport/client.rs
+++ src/layers/web_transport/client.rs
@@ -2,9 +2,21 @@
 //! 
 //! Contains a webtransport client that implements layer. Can only communicate with the host.
 
-use wtransport::{Endpoint, endpoint::endpoint_side::Client};
+use std::collections::HashMap;
 
-pub struct WebTransportClient {
+use wtransport::{Endpoint, endpoint::endpoint_side::Client, ClientConfig};
+
+pub struct WebTransportClient<NamespaceID: PartialEq + Hash> {
     /// The actual webtransport client
     client: Endpoint<Client>,
+    /// The array map of namespace channels
+    namespaces: HashMap<NamespaceID, ()>,
+}
+
+impl WebTransportClient {
+    pub fn new() {
+        let connection = Endpoint::client(ClientConfig::default()).unwrap()
+            .connect("127.0.0.1:8092").await
+            .unwrap();
+    }
 }
