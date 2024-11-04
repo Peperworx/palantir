bb3c4f904cd0a807d8a214704e9b9043239df8ef
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2024-11-03 21:20:30 -0500

    I think this is what the palantir struct needs. I am making it generic over backends so I can test it with a local backend based on something simple like channels.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,26 @@
 #[warn(clippy::pedantic)]
 #[allow(clippy::module_name_repetitions)]
 
+
+mod request;
+mod actor_id;
+use actor_id::ActorID;
+use request::Request;
+
+
+
+
+use std::collections::HashMap;
+use tokio::sync::{mpsc, RwLock};
+
+
+
+pub struct Palantir<B> {
+    /// This system's id
+    system_id: String,
+    /// The backend that is used by this palantir instance
+    /// to communicate with other systems.
+    backend: B,
+    /// A hashmap of message handling channels for actors
+    actor_handlers: RwLock<HashMap<ActorID, mpsc::Sender<Request>>>,
+}
